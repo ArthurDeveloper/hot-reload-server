27c6518bf493cebcfc362ae32d0e5090d3602ba2
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-11-26 19:34:17 -0300

    feat: Added package to npm registry

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ watchFiles(defaultPath, ['.html', '.css', '.js'], (curr, prev) => {
 	server.sockets.emit('fileChanged');
 });
 
-
 httpServer.listen(3000, () => {
 	console.log('Server started at http://localhost:3000');
 	console.log('Watching files at '+defaultPath);

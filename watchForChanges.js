387071b32c8e6202b57476e48e17fa04ec43c892
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-11-26 20:31:10 -0300

    feat: Now the hot reloader also watches for files under subdirectories of the watched directory

--- watchForChanges.js
+++ watchForChanges.js
@@ -6,11 +6,16 @@ function watchFiles(dirPath, extensions, callback) {
 
 	for (const file of files) {
 		const fileName = path.join(dirPath, file);
-	
-		for (const extension of extensions) {
-			if (fileName.indexOf(extension) !== -1) {
-				fs.watchFile(fileName, callback);
-				break;
+		const stat = fs.lstatSync(fileName);
+
+		if (stat.isDirectory()) {
+			watchFiles(fileName, extensions, callback);
+		} else {
+			for (const extension of extensions) {
+				if (fileName.indexOf(extension) !== -1) {
+					fs.watchFile(fileName, callback);
+					break;
+				}
 			}
 		}
 	}
